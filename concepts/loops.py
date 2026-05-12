fe078e56275d67cb5fac2995a3e3f3f9a2fe4868
Hrishikesh Shinde <shrishikesh925@gmail.com>
2026-05-12 16:08:33 +0000

    added the loop.py file

---
+++ concepts/loops.py
@@ -0,0 +1,23 @@
+print("Table of 5")
+
+for x in range(10):
+    print(f"5 * {x} = {5 * x}")
+    x += 1
+
+print("\n")
+
+print("Max has ")
+for i in range(1, 5):
+    print(i, "?")
+    if i == 4:
+        print("Yes")
+    else:
+        print("No")
+print("4 World championships and will win more")
+
+print("\n")
+
+count = 0
+while count < 5:
+    print(count)
+    count += 1
