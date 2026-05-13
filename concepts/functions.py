c2393ba2aaa683f00c416fd9906bbb25d0401b6c
Hrishikesh Shinde <shrishikesh925@gmail.com>
2026-05-13 18:20:11 +0000

    functions in py is really simple

---
+++ concepts/functions.py
@@ -0,0 +1,11 @@
+def championshipcounts(value):
+    if(value == 4):
+        print("Ohh Its Max verstappen and Sebastian Vettel a better and and dominant driver personally my favorite")
+    elif(value == 7):
+        print("Okay do that is Lewis and Michael Schumacher, good for you but stay away from me please")
+    else:
+        print("Invalid choice")
+
+value = int(input("Please choose between 4 and 7 times of world champion: "))
+championshipcounts(value)
+print("Ok bye")
