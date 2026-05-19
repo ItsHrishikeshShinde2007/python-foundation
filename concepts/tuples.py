4e51c43e947f52a6e788e9ceaa81a4cbcca3cf1a
Hrishikesh Shinde <shrishikesh925@gmail.com>
2026-05-19 16:27:39 +0000

    Tuples is unnecessary as per me

---
+++ concepts/tuples.py
@@ -0,0 +1,8 @@
+print("Alright guys so this is the tuples file and below is a tuple printed. " \
+"\n\nNow while you cannot understand much difference between lists and tuples just from output so let me help you" \
+"In lists we add data in square brackets '[' and ']' and in tuples we instead use '(' and ')'" \
+"So the main difference between the lists and tuples is that the list is dynamic you can add more values " \
+"Tuples are static, you cannot change the position of elements and not change the elements itself.\n")
+champions = ("Max Verstappen", "Lewis Hamilton", "Sebastian Vettel")
+print("The Tuples are:")
+print(champions)
