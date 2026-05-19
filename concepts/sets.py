9f5a504ccd63c1a74e1b4a47c30af511bc6cb79d
Hrishikesh Shinde <shrishikesh925@gmail.com>
2026-05-19 16:43:22 +0000

    Sets also felt weird buit fine

---
+++ concepts/sets.py
@@ -0,0 +1,6 @@
+subjects = {"Data Structures", "Data Structures","Computer Science", "Structured programming", "OOP programming"}
+subjects.add("biology")
+print("\nSets are unordered collections of unique values")
+print("Even the sets can add or delete the elements it just does not have index or else its very similar to lists")
+print("\nThe Sets are: ")
+print(subjects)
