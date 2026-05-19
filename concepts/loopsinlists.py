598086bf02146786e137cfea3c0a52f6e8ada6d5
Hrishikesh Shinde <shrishikesh925@gmail.com>
2026-05-19 15:09:39 +0000

    Got back from a major loop

---
+++ concepts/loopsinlists.py
@@ -0,0 +1,11 @@
+drivers = ["Lando Norris", "Max Verstappen", "Oscar Piastri"]
+
+print("Championship contenders of 2025 are: ")
+for i in range(len(drivers)):
+    print("Position: " , i + 1, drivers[i])
+
+drivers = ["Max Verstappen", "Oscar Piastri", "Lando Norris"]
+print("\nBut in my heart the drivers who really are worth of Championship are")
+for name in drivers:
+    print(name)
+print("\n")
