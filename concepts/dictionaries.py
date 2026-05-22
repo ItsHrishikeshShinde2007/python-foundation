f6fccc56e90643a108458558b2a0a23d12da1606
Hrishikesh Shinde <shrishikesh925@gmail.com>
2026-05-22 11:55:10 +0000

    New concept added thats nice

---
+++ concepts/dictionaries.py
@@ -0,0 +1,24 @@
+print("My favirite F1 driver is: ")
+
+myfavdriver1 = {
+    "Name" : "Max Verstappen",
+    "Racing Team" : "Red Bull Racing",
+    "World Championship" : 4,
+}
+
+for key, value in myfavdriver1.items():
+    print(key, ":" ,value)
+
+YorN = input("Do you also wanna create a dictionary by inputing your favorite driver details in (Y/N)?")
+if (YorN == "Y" or YorN ==  "y"):
+    myfavdriver2 = {}
+    myfavdriver2["Name"] = input("Enter driver name: ")
+    myfavdriver2["Racing Team"] = input("Enter his racing team name: ")
+    myfavdriver2["Driver Number"] = int(input("Enter his driver number: "))
+    print("Ok so your favorite Driver is:-")
+    for key, value in myfavdriver2.items():
+        print(key, ":" ,value)
+elif (YorN == "N" or YorN == "n"):
+    print("Fine")
+else:
+    print("Alright nevermind")
