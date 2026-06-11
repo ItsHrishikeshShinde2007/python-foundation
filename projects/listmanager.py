354a6f9d456771e56b552fff62a91194f7a8a081
Hrishikesh Shinde <shrishikesh925@gmail.com>
2026-06-11 16:28:32 +0000

    Rebuilt Python manager with functions

--- projects/listmanager.py
+++ projects/listmanager.py
@@ -1,44 +1,51 @@
-Listmanager = []
-print("Welcome to List manager app")
-while(True):
-    print("Choose from the below")
-    print("a. Add a Task")
-    print("b. Delete a completed task")
-    print("c. List all the task")
-    print("d. Exit")
-    choice = input("")
-    if(choice == 'a'):
-        task = str(input("State the task that you want to add: "))
-        Listmanager.append(task)
-        print()
-    elif(choice == 'b'):
-        if(len(Listmanager) == 0):
-            print("There are no tasks to list")
+items = []
+
+def add_item():
+    new_item = input("Enter the item you want to add: ")
+    items.append(new_item)
+    print("ok so new item " + new_item + " added successfully. Updated list:")
+    show_items()
+
+def remove_item():
+    if not items:
+        print("The list is empty. There is nothing to remove.")
+    else:
+        print("This is the list again look at the list and state which item you want to remove: ")
+        show_items()
+        print("Enter the number of the item you want to remove: ")
+        remove_index = int(input())
+        if (remove_index > len(items)) or (remove_index < 1):
+            print("Invalid number. Please enter a number between 1 and", len(items))
         else:
-            for i in range(len(Listmanager)):
-                print("Task " , i + 1 ,":", Listmanager[i])
-            print()
-            delindex = int(input("Please State task number that you want to delete:"))
-            if(delindex < 1 or delindex > len(Listmanager)):
-                print("The Task does not exists")
-            else:
-                print("Ok so the task " + Listmanager[delindex - 1] + " is deleted")
-                Listmanager.pop(delindex - 1)
-                print()
-                print("The revised list is:- ")
-                for i in range(len(Listmanager)):
-                    print("Task " , i + 1 ,":", Listmanager[i])
-            print()
-    elif(choice == 'c'):
-        if(len(Listmanager) == 0):
-            print("No list in the To do list to show")
-        else:
-            print("The List of all the tasks are:-")
-            for i in range(len(Listmanager)):
-                 print("Task " , i + 1 ,":", Listmanager[i])
-        print()
-    elif(choice == 'd'):
-        print("Simply lovely")
+            removed_item = items.pop(remove_index - 1)
+            print("ok so item " + removed_item + " removed successfully. Updated list:")
+            show_items()
+
+def show_items():
+    if len(items) == 0:
+        print("The list is empty.")
+    else:
+        print("The list is: ")
+        for i in range(len(items)):
+            print(i + 1, items[i])
+
+print("Welcome to List Manager")
+while True:
+    print("What operation you want to perform? (Add / Remove)")
+    print("1. Add")
+    print("2. Remove")
+    print("3. Show")
+    print("4. Exit")
+    print("Enter the option number (1, 2, 3 or 4): ")
+    operation = input()
+    if operation == "1":
+        add_item()
+    elif operation == "2":
+        remove_item()
+    elif operation == "3":
+        show_items()
+    elif operation == "4":
+        print("Simply Lovely")
         break
     else:
-        print("Invalid Choice")
+        print("Invalid option. Please enter 1, 2, 3, or 4.")
